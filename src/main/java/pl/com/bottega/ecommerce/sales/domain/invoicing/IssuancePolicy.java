417d5e8b51669964f5c6d43b28fbc6255c79b5a5
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-03-10 22:40:38 +0100

    Added StandardIssuancePolicy class.

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/IssuancePolicy.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/IssuancePolicy.java
@@ -1,4 +1,9 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
 public interface IssuancePolicy {
+    Money countTaxValue(RequestItem item);
+    Tax getTax(RequestItem item);
 }
