3e197eaa857eb2a6dca8e570eeda0e72fff55c06
S-T-A-L-K-R <name.generate@gmail.com>
2022-03-10 11:01:43 +0100

    Created & implemented "TaxInterface" interface to handle various taxes

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
@@ -19,36 +19,13 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 public class BookKeeper {
     InvoiceFactory factory = new InvoiceFactory();
     // public Invoice issuance(ClientData client, List<RequestItem> items) {
-    public Invoice issuance(InvoiceRequest request) {
+    public Invoice issuance(InvoiceRequest request, TaxInterface taxInterface) {
         Invoice invoice = factory.newInvoice(request.getClient());
 
         for (RequestItem item : request.getItems()) {
             Money net = item.getTotalCost();
-            BigDecimal ratio = null;
-            String desc = null;
 
-            switch (item.getProductData().getType()) {
-                case DRUG:
-                    ratio = BigDecimal.valueOf(0.05);
-                    desc = "5% (D)";
-                    break;
-                case FOOD:
-                    ratio = BigDecimal.valueOf(0.07);
-                    desc = "7% (F)";
-                    break;
-                case STANDARD:
-                    ratio = BigDecimal.valueOf(0.23);
-                    desc = "23%";
-                    break;
-
-                default:
-                    throw new IllegalArgumentException(item.getProductData().getType() + " not handled");
-            }
-
-            Money taxValue = net.multiplyBy(ratio);
-
-            Tax tax = new Tax(taxValue, desc);
-
+            Tax tax = taxInterface.getTax(item.getProductData().getType(), net);
             InvoiceLine invoiceLine = new InvoiceLine(item.getProductData(), item.getQuantity(), net, tax);
             invoice.addItem(invoiceLine);
         }
